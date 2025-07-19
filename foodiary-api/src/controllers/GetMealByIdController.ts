f890d6c4bb82f566a478a95bd973eab5ba65cc6a
Laercio Calheiros <laercio.calheiros@easycarros.com>
2025-07-19 16:03:33 -0300

    Create get meal by id'

---
+++ foodiary-api/src/controllers/GetMealByIdController.ts
@@ -0,0 +1,36 @@
+import { and, eq } from 'drizzle-orm';
+import z from 'zod';
+import { db } from '../db';
+import { mealsTable } from '../db/schema';
+import { HttpResponse, ProtectedHttpRequest } from '../types/Http';
+import { badRequest, ok } from '../utils/http';
+
+const schema = z.object({
+  mealId: z.uuid(),
+});
+
+export class GetMealByIdController {
+  static async handle({
+    userId,
+    params,
+  }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(params);
+
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
+
+    const meals = await db.query.mealsTable.findFirst({
+      columns: {
+        id: true,
+        foods: true,
+        createdAt: true,
+        icon: true,
+        name: true,
+      },
+      where: and(eq(mealsTable.id, data.mealId), eq(mealsTable.userId, userId)),
+    });
+
+    return ok({ meals });
+  }
+}
