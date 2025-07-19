e720c9c4a43cba3d405b7b1dae8f52db08f46aba
Laercio Calheiros <laercio.calheiros@easycarros.com>
2025-07-19 18:34:23 -0300

    Send file key to aws sqs

---
+++ foodiary-api/src/functions/fileUploadedEvent.ts
@@ -0,0 +1,18 @@
+import { SendMessageCommand } from '@aws-sdk/client-sqs';
+import { S3Event } from 'aws-lambda';
+import { sqsClient } from '../clients/sqsClients';
+
+export async function handler(event: S3Event) {
+  await Promise.all(
+    event.Records.map(async (record) => {
+     const command = new SendMessageCommand({
+      QueueUrl: process.env.MEALS_QUEUE,
+      MessageBody: JSON.stringify({
+        fileKey: record.s3.object.key
+      })
+     })
+
+     await sqsClient.send(command);
+    })
+  );
+}
